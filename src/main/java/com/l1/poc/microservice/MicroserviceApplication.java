8d36d0bfcc42819cc433c3765a23b1187f056830
MichaelLifshitsL1 <mlifshits@loyalty.com>
2018-10-10 18:07:49 -0400

    initial commit

---
+++ src/main/java/com/l1/poc/microservice/MicroserviceApplication.java
@@ -0,0 +1,12 @@
+package com.l1.poc.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceApplication.class, args);
+	}
+}
